8c39cdba62e54da46c0a0ce22457932703d5e81f
Cristi Scheye <cristi.scheye@gmail.com>
2013-09-22 09:09:07 -0700

    Adding helper for score check

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ set :sessions, true
 
 helpers do
 
-  def score(hand)
+  def calculate_score(hand)
     values = hand.map{ |card| card[0]}
 
     score = 0
@@ -22,13 +22,55 @@ helpers do
 
     # Correct for Aces
     values.select{|val| val == 'A'}.count.times do
-      break if score <= Blackjack::BLACKJACK_AMOUNT
+      break if score <= 21
       score -= 10
     end
 
     score
   end
 
+  def is_busted?(hand)
+    calculate_score(hand) > 21
+  end
+
+  def is_blackjack?(hand)
+    calculate_score(hand) == 21
+  end
+
+  def end_game_variables
+    @player_turn = false
+    @dealer_turn = false
+    @game_over = true
+  end
+
+  def check_player_score(player_hand)
+    if is_busted?(session[:player_cards])
+      @msg = "Sorry #{session[:player_name]}, you busted. You lose :("
+      end_game_variables
+    elsif is_blackjack?(session[:player_cards])
+      @msg = "Congrats #{session[:player_name]}, you got blackjack! You win :)"
+      end_game_variables
+    end
+  end
+
+  def check_dealer_score(dealer_hand)
+    if calculate_score(session[:dealer_cards]) < 17
+      @dealer_turn = true
+    elsif is_busted?(session[:dealer_cards])
+      @msg = "Dealer busted! You win :)"
+      end_game_variables
+    elsif is_blackjack?(session[:dealer_cards])
+      @msg = 'Dealer hit blackjack. You lose :('
+      end_game_variables
+    end
+  end
+
+end
+
+before do
+  @player_turn = true
+  @dealer_turn = false
+  @game_over = false
 end
 
 get '/' do
@@ -58,7 +100,7 @@ get '/home' do
   erb :home
 end
 
-get '/new_blackjack' do
+get '/blackjack/new' do
   #setup deck
   values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
   suits = ['Spades', 'Diamonds', 'Hearts', 'Clubs']
@@ -74,19 +116,40 @@ get '/new_blackjack' do
     session[:player_cards] << session[:deck].pop
     session[:dealer_cards] << session[:deck].pop
   end
-  redirect '/blackjack'
+
+  redirect '/blackjack' # Keeps user from getting new cards at a screen refresh
 end
 
 get '/blackjack' do
-  session[:player_score] = score(session[:player_cards])
+  check_player_score(session[:player_cards])
   erb :blackjack
 end
 
 post '/blackjack/player_hit' do
+  @msg = 'You chose to hit'
   session[:player_cards] << session[:deck].pop
-  redirect '/blackjack'
+
+  check_player_score(session[:player_cards])
+
+  # declare winner, adjust purse
+ 
+  erb :blackjack
 end
 
 post '/blackjack/player_stay' do
-  redirect '/blackjack'
+  @msg = 'You chose to stay.'
+  @player_turn = false
+
+  check_dealer_score(session[:dealer_cards])
+  # declare winner, adjust purse
+
+  erb :blackjack
+end
+
+post '/blackjack/dealer_hit' do
+  session[:dealer_cards] << session[:deck].pop
+  @player_turn = false
+  check_dealer_score([:dealer_cards])
+  
+  erb :blackjack
 end
